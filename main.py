c10e7d1c8bd23831e6300a671459aa60b334c855
A.Kleinhans <alex.kleinhans@gmx.de>
2019-06-29 14:29:52 +0200

    seperate threads
    added seperate threads for server and ttn input fetching

---
+++ main.py
@@ -0,0 +1,31 @@
+import threading
+
+from classes.server import Server
+from http.server import HTTPServer
+
+from classes.input.ttn import InputTTN
+
+print("Start")
+
+class APIThread(threading.Thread):
+    def run(self):
+        print("Run API-Server...")
+        httpServer = HTTPServer(('127.0.0.1', 8081), Server)
+        httpServer.serve_forever(poll_interval=0.5)
+print("1")
+
+class InputThread(threading.Thread):
+    def run(self):
+        print("Run InputTTN...")
+        ttnInstance = InputTTN()
+        ttnInstance.fetch()
+print("2")
+
+def main():
+    api_thread = APIThread()
+    api_thread.start()  # ...Start the thread, invoke the run method
+    print("3")
+    input_thread = InputThread()
+    input_thread.start()  # ...Start the thread, invoke the run method
+
+main()
