5526abd22cb8db81adad961017661fbbf763751b
David Dreher <dredav@users.noreply.github.com>
2019-06-29 15:30:30 +0200

    adjust API json format

--- classes/server.py
+++ classes/server.py
@@ -1,6 +1,7 @@
 import sqlite3
 from http.server import BaseHTTPRequestHandler
 import json
+from decimal import Decimal
 
 
 class Server(BaseHTTPRequestHandler):
@@ -10,22 +11,26 @@ class Server(BaseHTTPRequestHandler):
         self.send_header('Content-type', 'application/json')
         self.end_headers()
 
-        result = []
+        result = {}
+        result['locations'] = []
 
         with sqlite3.connect('data/storage.sqlite3') as connection:
             cursor = connection.cursor()
 
-            cursor.execute('select location.name, input.value, input.timestamp, max(input.timestamp) from input join location on input.deviceId = location.deviceId and input.source = location.source group by input.source, input.deviceId;')
+            cursor.execute('select location.name, location.capacity, location.image, input.value, input.timestamp, max(input.timestamp) from input join location on input.deviceId = location.deviceId and input.source = location.source group by input.source, input.deviceId;')
 
             for entry in cursor.fetchall():
                 print(entry)
                 resultEntry = {}
 
                 resultEntry['name'] = entry[0]
-                resultEntry['visitors'] = entry[1]
-                resultEntry['lastTimestamp'] = entry[2]
+                resultEntry['capacity'] = entry[1]
+                resultEntry['image'] = entry[2]
+                resultEntry['pressure'] = str(min(1, Decimal(entry[3]) / Decimal(entry[1])))
+                resultEntry['visitors'] = entry[3]
+                resultEntry['lastTimestamp'] = entry[4]
 
-                result.append(resultEntry)
+                result['locations'].append(resultEntry)
 
         self.wfile.write(bytes(str(json.dumps(result)), "utf8"))
         return
