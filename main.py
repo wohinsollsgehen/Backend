99b61eea3fedbeafd3ad52e8568913b790e46417
David Dreher <dredav@users.noreply.github.com>
2019-06-29 14:55:41 +0200

    remove debug output

--- main.py
+++ main.py
@@ -12,19 +12,16 @@ class APIThread(threading.Thread):
         print("Run API-Server...")
         httpServer = HTTPServer(('127.0.0.1', 8081), Server)
         httpServer.serve_forever(poll_interval=0.5)
-print("1")
 
 class InputThread(threading.Thread):
     def run(self):
         print("Run InputTTN...")
         ttnInstance = InputTTN()
         ttnInstance.fetch()
-print("2")
 
 def main():
     api_thread = APIThread()
     api_thread.start()  # ...Start the thread, invoke the run method
-    print("3")
     input_thread = InputThread()
     input_thread.start()  # ...Start the thread, invoke the run method
 
